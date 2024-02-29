96c1208feb23230c80995e53a11860056641d6b3
JYOTIPATIL-hub <142783664+JYOTIPATIL-hub@users.noreply.github.com>
2024-02-29 21:41:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function toFahrenheit(celsius) {
  // Write your code here
+	// In your scrip
+    // Get the input values from the form
+   
+  return ((n * 9.0 / 5.0) + 32.0);  
 }
 
 // Do not change the code below
